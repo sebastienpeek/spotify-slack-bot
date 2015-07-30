b8f4a76e61dbbbbdc7700bd3f08b8ebcba3781de
Sebastien Peek <sebastien.peek@gmail.com>
2015-07-31 02:19:55 +1000

    Updated if statements to not have unnecessary brackets around OR statements, lel

--- responder.js
+++ responder.js
@@ -19,28 +19,27 @@ Responder = (function() {
 
 				if (~originalText.indexOf(botId)) {
 					// message to spotify bot directly
-					if (~text.indexOf("now playing") || ~text.indexOf("currently playing")) {
-
-						scriptrunner.getState(function(err, state) {
-							if (~state.indexOf("playing")) {
-								scriptrunner.getTrack(function(err, track) { 
-									var artist = track.artist;
-									var name = track.name;
-									res("Currently listening to " + name + " by " + artist);
-								});
-							} else {
-								res("Spotify is currently "+ state);
-							};
-						})
-
-					} else if (~text.indexOf("stop song") || (~text.indexOf("stop playing")) || 
-						(~text.indexOf("stop"))) {
+					if (~text.indexOf("now playing") || ~text.indexOf("currently playing") || 
+						~text.indexOf("current")) {
+							scriptrunner.getState(function(err, state) {
+								if (~state.indexOf("playing")) {
+									scriptrunner.getTrack(function(err, track) { 
+										var artist = track.artist;
+										var name = track.name;
+										res("Currently listening to " + name + " by " + artist);
+									});
+								} else {
+									res("Spotify is currently "+ state);
+								};
+							})
+					} else if (~text.indexOf("stop song") || ~text.indexOf("stop playing") || 
+						~text.indexOf("stop")) {
 							scriptrunner.stop(function(callback) {
 								res("Stopping!");
 							});
-					} else if (~text.indexOf("start song") || (~text.indexOf("start playing")) ||
-						(~text.indexOf("play song")) || (~text.indexOf("play")) || 
-						(~text.indexOf("start"))) {
+					} else if (~text.indexOf("start song") || ~text.indexOf("start playing") ||
+						~text.indexOf("play song") || ~text.indexOf("play") || 
+						~text.indexOf("start")) {
 							// Check to make sure the user isn't defining what track to play next!
 							if (~text.indexOf("spotify:")) {
 								var trackId = originalText.replace(/.*\<|\>/gi,'');
@@ -62,8 +61,8 @@ Responder = (function() {
 								});
 							}
 
-					} else if (~text.indexOf("next song") || (~text.indexOf("next")) || 
-						(~text.indexOf("forward")) || (~text.indexOf("skip"))) {
+					} else if (~text.indexOf("next song") || ~text.indexOf("next") || 
+						~text.indexOf("forward") || ~text.indexOf("skip")) {
 							scriptrunner.next(function(callback) {
 								scriptrunner.getTrack(function(err, track) { 
 										var artist = track.artist;
@@ -71,8 +70,8 @@ Responder = (function() {
 										res("Now playing " + name + " by " + artist);
 									});
 							});
-					} else if (~text.indexOf("previous song") || (~text.indexOf("previous")) || 
-						(~text.indexOf("back"))) {
+					} else if (~text.indexOf("previous song") || ~text.indexOf("previous") || 
+						~text.indexOf("back")) {
 
 							scriptrunner.stop(function(callback) {
 								scriptrunner.previous(function(callback) {
