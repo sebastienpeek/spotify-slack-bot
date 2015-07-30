c6bca3904a461ae3be2eda515f6ec32538a2d9f6
Sebastien Peek <sebastien.peek@gmail.com>
2015-07-31 02:12:35 +1000

    Updated botId detection, no more hardcoding. Changed to detect full state of playing. Added in new keywords for starting, stopping, playing, next, and going back. Updated previous handling to work, weird Spotify issue of going to previous track. It all has something to do with how much of the song has been played... Maybe check the position and then work out if one previous call is needed or two.

--- responder.js
+++ responder.js
@@ -6,24 +6,23 @@ Responder = (function() {
 
 	var scriptrunner = new ScriptRunner();
 
-	Responder.prototype.respondToMessage = function(message, userObject, slackName, res) {
+	Responder.prototype.respondToMessage = function(message, userObject, botId, res) {
 		console.log("respondToMessage() - " + message);
 		// Lowercase version for easy parsing
 		var text = message.text.toLowerCase();
-		// Orginal version for track id parsing
+		// Orginal version for botId detection and spotify uri parsing
 		var originalText = message.text;
 
 		if (message.type === 'message' && 
 			(text != null) && 
-			(message.channel != null) && 
-			(slackName != userObject.name)) {
+			(message.channel != null)) {
 
-				if (~text.indexOf("u08cg0kcp")) {
+				if (~originalText.indexOf(botId)) {
 					// message to spotify bot directly
 					if (~text.indexOf("now playing") || ~text.indexOf("currently playing")) {
 
 						scriptrunner.getState(function(err, state) {
-							if (~state.indexOf("play")) {
+							if (~state.indexOf("playing")) {
 								scriptrunner.getTrack(function(err, track) { 
 									var artist = track.artist;
 									var name = track.name;
@@ -34,49 +33,60 @@ Responder = (function() {
 							};
 						})
 
-					} else if (~text.indexOf("stop song") || (~text.indexOf("stop playing"))) {
-						scriptrunner.stop(function(callback) {
-							res("Stopping!");
-						});
-					} else if (~text.indexOf("start song") || (~text.indexOf("start playing")) || (~text.indexOf("play song"))) {
-
-						// Check to make sure the user isn't defining what track to play next!
-						if (~text.indexOf("spotify:")) {
-							var trackId = originalText.replace(/.*\<|\>/gi,'');
-							scriptrunner.playSong(trackId, function(callback) {
-								scriptrunner.getTrack(function(err, track) { 
-									var artist = track.artist;
-									var name = track.name;
-									res("Now playing " + name + " by " + artist);
+					} else if (~text.indexOf("stop song") || (~text.indexOf("stop playing")) || 
+						(~text.indexOf("stop"))) {
+							scriptrunner.stop(function(callback) {
+								res("Stopping!");
+							});
+					} else if (~text.indexOf("start song") || (~text.indexOf("start playing")) ||
+						(~text.indexOf("play song")) || (~text.indexOf("play")) || 
+						(~text.indexOf("start"))) {
+							// Check to make sure the user isn't defining what track to play next!
+							if (~text.indexOf("spotify:")) {
+								var trackId = originalText.replace(/.*\<|\>/gi,'');
+								scriptrunner.playSong(trackId, function(callback) {
+									scriptrunner.getTrack(function(err, track) { 
+										var artist = track.artist;
+										var name = track.name;
+										res("Now playing " + name + " by " + artist);
+									});
+								});	
+							
+							} else {
+								scriptrunner.play(function(callback) {
+									scriptrunner.getTrack(function(err, track) { 
+											var artist = track.artist;
+											var name = track.name;
+											res("Now playing " + name + " by " + artist);
+										});
 								});
-							});	
-						
-						} else {
-							scriptrunner.play(function(callback) {
+							}
+
+					} else if (~text.indexOf("next song") || (~text.indexOf("next")) || 
+						(~text.indexOf("forward")) || (~text.indexOf("skip"))) {
+							scriptrunner.next(function(callback) {
 								scriptrunner.getTrack(function(err, track) { 
 										var artist = track.artist;
 										var name = track.name;
 										res("Now playing " + name + " by " + artist);
 									});
 							});
-						}
+					} else if (~text.indexOf("previous song") || (~text.indexOf("previous")) || 
+						(~text.indexOf("back"))) {
 
-					} else if (~text.indexOf("next song")) {
-						scriptrunner.next(function(callback) {
-							scriptrunner.getTrack(function(err, track) { 
-									var artist = track.artist;
-									var name = track.name;
-									res("Now playing " + name + " by " + artist);
-								});
-						});
-					} else if (~text.indexOf("previous song")) {
-						scriptrunner.previous(function(callback) {
-							scriptrunner.getTrack(function(err, track) { 
-									var artist = track.artist;
-									var name = track.name;
-									res("Now playing " + name + " by " + artist);
-								});
-						});
+							scriptrunner.stop(function(callback) {
+								scriptrunner.previous(function(callback) {
+									scriptrunner.previous(function(callback) {
+										scriptrunner.play(function(callback) {
+											scriptrunner.getTrack(function(err, track) { 
+												var artist = track.artist;
+												var name = track.name;
+												res("Now playing " + name + " by " + artist);
+											});
+										})	
+									})
+								})
+							})
 					}
 				};
 		} else {
